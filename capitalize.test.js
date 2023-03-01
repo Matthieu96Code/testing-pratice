894fe447e90a267d73054687f82557cc74e54e62
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-01 07:03:35 +0000

    add capitalize test

---
+++ capitalize.test.js
@@ -0,0 +1,12 @@
+const capitalize = require('./capitalize')
+
+test('properly capitalize first letter of string', () => {
+  // Arrange
+  const strOne = 'happy coding!'
+
+  // Act
+  const resultStrOne = capitalize(strOne)
+
+  // Assert
+  expect(resultStrOne).toBe('Happy coding')
+})
