e9c0b1e8a1df512402f0035afd8e2359f79e71d0
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-01 06:01:21 +0000

    add calculator class file

---
+++ calculator.js
@@ -0,0 +1,21 @@
+class calculator {
+
+  add (number1, number2){
+    return number1 + number2
+  }
+
+  subtract (number1, number2){
+    return number1 - number2
+  }
+
+  divide (number1, number2){
+    return number1 / number2
+  }
+  
+  multiply (number1, number2){
+    return number1 * number2
+  }
+
+}
+
+module.exports = calculator
